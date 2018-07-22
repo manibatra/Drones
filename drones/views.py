e3e974d10cef1f8b0245eac9a4c5b7b405fd345f
Mani Batra <manibatra2002@gmail.com>
2018-07-22 18:32:23 +1000

    added map to dashboard

--- drones/views.py
+++ drones/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import os
+import json
 
 # Create your views here.
 def index(request):
@@ -10,4 +12,7 @@ def stimulator(request):
 
 
 def dashboard(request):
-	return render(request, 'drones/dashboard.html', {})
+	map_api_key = os.environ['MAP_KEY']
+	return render(request, 'drones/dashboard.html', {
+		'map_api_key' : map_api_key
+		})
