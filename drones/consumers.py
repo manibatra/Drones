d8250cf38c9f8493b0f224d7886dac19a1c9e2f0
Mani Batra <manibatra2002@gmail.com>
2018-07-22 15:31:29 +1000

    made the consumers asynchronous

--- drones/consumers.py
+++ drones/consumers.py
@@ -1,30 +1,29 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class DroneConsumer(WebsocketConsumer):
-    def connect(self):
+class DroneConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.group_name = "drone_group"
 
         # Join drone group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave drone group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
         # Send message to dashboard group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             "dashboard_group",
             {
                 'type': 'drone_message',
@@ -33,29 +32,29 @@ class DroneConsumer(WebsocketConsumer):
         )
 
 
-class DashboardConsumer(WebsocketConsumer):
-    def connect(self):
+class DashboardConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.group_name = "dashboard_group"
 
         # Join dashboard group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave dashboard group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
     # Receive message from drone group
-    def drone_message(self, event):
+    async def drone_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
